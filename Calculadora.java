fa839cbbe4d7856b13e8a46192d19896733e0f16
Marcia Menezes <marcia.menezes.92@hotmail.com>
2020-03-05 18:23:59 -0300

    Calculadora completa

--- Calculadora.java
+++ Calculadora.java
@@ -9,18 +9,38 @@ public class Calculadora {
 		int num1=0;
 		int num2=0;
 		int total;
+		int i=0;
 		String aux;
+		String tpoperacao = null;
 		
 		aux = JOptionPane.showInputDialog ("Digite um numero");
 		num1 = Integer.parseInt(aux);
 		
+		aux = JOptionPane.showInputDialog ("Digite a operacao");
+		tpoperacao = aux;
+		
 		aux = JOptionPane.showInputDialog("Digite outro numero");
 		num2 = Integer.parseInt(aux);
-		
-		total = num1+num2;
-		
-		JOptionPane.showMessageDialog  (null,"A soma e: " + total);		
 
+		if(tpoperacao.equals ("+"))
+		{
+			total = num1+num2;
+			JOptionPane.showMessageDialog(null,"A soma e: "+total);
+		}
+		else if (tpoperacao.equals ("-"))
+		{
+			total = num1-num2;
+			JOptionPane.showMessageDialog(null,"A subtracao e: "+total);
+		}
+		else if (tpoperacao.equals ("*"))
+		{
+			total = num1*num2;
+			JOptionPane.showMessageDialog(null,"A multiplicacao e: "+total);
+		}	
+		else 
+		{
+			total = num1/num2;
+			JOptionPane.showMessageDialog(null,"A divisao e: "+total);
+		}		
 	}
-
 }
